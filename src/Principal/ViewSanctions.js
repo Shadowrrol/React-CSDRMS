bf3d6f65a7232ed6ea4e361eeb2caededb586bbb
Andreiyaa1 <143610003+Andreiyaa1@users.noreply.github.com>
2024-05-20 16:10:14 +0800

    view sanction added css

--- src/Principal/ViewSanctions.js
+++ src/Principal/ViewSanctions.js
@@ -1,23 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import styles from '../Navigation.module.css'; // Import CSS module
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import styles from "../Navigation.module.css"; // Import CSS module
 
-import LocalPoliceIcon from '@mui/icons-material/LocalPolice';
-import AssessmentIcon from '@mui/icons-material/Assessment';
+import AssessmentIcon from "@mui/icons-material/Assessment";
+import LocalPoliceIcon from "@mui/icons-material/LocalPolice";
 
 const ViewSanctions = () => {
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
   const [sanctions, setSanctions] = useState([]);
 
   useEffect(() => {
     const fetchSanctions = async () => {
       try {
-        const response = await axios.get('http://localhost:8080/sanction/getAllPendingSanctions');
-        console.log('Fetched sanctions:', response.data);
+        const response = await axios.get("http://localhost:8080/sanction/getAllPendingSanctions");
+        console.log("Fetched sanctions:", response.data);
         setSanctions(response.data);
       } catch (error) {
-        console.error('Error:', error);
+        console.error("Error:", error);
       }
     };
     fetchSanctions();
@@ -25,21 +25,21 @@ const ViewSanctions = () => {
 
   const handleLogout = () => {
     // Clear the authentication token from localStorage
-    localStorage.removeItem('authToken');
+    localStorage.removeItem("authToken");
     // Redirect the user to the login page
-    navigate('/');
+    navigate("/");
   };
 
   const handleApprove = async (sanctionId) => {
     try {
-      const response = await axios.post('http://localhost:8080/sanction/approveSanction', null, {
+      const response = await axios.post("http://localhost:8080/sanction/approveSanction", null, {
         params: {
-          sanctionId: sanctionId
-        }
+          sanctionId: sanctionId,
+        },
       });
       console.log(response.data); // log response from the server
       // Refresh sanctions after approval
-      const updatedSanctions = sanctions.map(sanction => {
+      const updatedSanctions = sanctions.map((sanction) => {
         if (sanction.sanction_id === sanctionId) {
           sanction.isApproved = 1;
         }
@@ -47,79 +47,170 @@ const ViewSanctions = () => {
       });
       setSanctions(updatedSanctions);
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
     }
   };
-  
-  
+
   const handleDecline = async (sanctionId) => {
     try {
-        const response = await axios.post('http://localhost:8080/sanction/declineSanction', null, {
-          params: {
-            sanctionId: sanctionId
-          }
-        });
-        console.log(response.data); // log response from the server
-        // Refresh sanctions after approval
-        const updatedSanctions = sanctions.map(sanction => {
-          if (sanction.sanction_id === sanctionId) {
-            sanction.isApproved = 2;
-          }
-          return sanction;
-        });
-        setSanctions(updatedSanctions);
-      } catch (error) {
-        console.error('Error:', error);
-      }
+      const response = await axios.post("http://localhost:8080/sanction/declineSanction", null, {
+        params: {
+          sanctionId: sanctionId,
+        },
+      });
+      console.log(response.data); // log response from the server
+      // Refresh sanctions after approval
+      const updatedSanctions = sanctions.map((sanction) => {
+        if (sanction.sanction_id === sanctionId) {
+          sanction.isApproved = 2;
+        }
+        return sanction;
+      });
+      setSanctions(updatedSanctions);
+    } catch (error) {
+      console.error("Error:", error);
+    }
   };
 
   return (
-    <div className={styles.wrapper} style={{ backgroundImage: 'url(/public/image-2-3@2x.png)' }}>
+    <div className={styles.wrapper} style={{ backgroundImage: "url(/public/image-2-3@2x.png)" }}>
       <div className={styles.sidenav}>
-        <img src="/image-removebg-preview (1).png" alt="" className={styles['sidebar-logo']}/>
-        <Link to="/report" className={styles['styled-link']}>
-          <AssessmentIcon className={styles.icon} /> 
-          <span className={styles['link-text']}>Report</span> 
+        <img src="/image-removebg-preview (1).png" alt="" className={styles["sidebar-logo"]} />
+        <Link to="/report" className={styles["styled-link"]}>
+          <AssessmentIcon className={styles.icon} />
+          <span className={styles["link-text"]}>Report</span>
         </Link>
-        <Link to="/viewSanctions" className={styles['styled-link']}>
-          <LocalPoliceIcon className={styles.icon} /> 
-          <span className={styles['link-text']}>Sanctions</span> 
+        <Link to="/viewSanctions" className={styles["styled-link"]}>
+          <LocalPoliceIcon className={styles.icon} />
+          <span className={styles["link-text"]}>Sanctions</span>
         </Link>
-        <button className={styles['logoutbtn']} onClick={handleLogout}>Logout</button>
+        <button className={styles["logoutbtn"]} onClick={handleLogout}>
+          Logout
+        </button>
       </div>
-      <div className='content'>
+      <div className="content">
         <h1>Sanctions</h1>
-        <div>
-          <h2>All Sanctions</h2>
-          <ul>
-            {sanctions.map(sanction => (
-              <li key={sanction.sanction_id}>
-                {/* Display sanction details */}
-                <div>Sanction ID: {sanction.sanction_id}</div>
-                <div>Student Name: {sanction.student.firstname}  {sanction.student.lastname}</div>
-                <div>Behavior Details: {sanction.behaviorDetails}</div>
-                <div>Sanction Recommendation: {sanction.sanctionRecommendation}</div>
-                {/* Approve and decline buttons */}
-                {sanction.isApproved !== 1 && (
-                  <div>
-                    <button onClick={() => handleApprove(sanction.sanction_id)}>Approve</button>
-                    <button onClick={() => handleDecline(sanction.sanction_id)}>Decline</button>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "column",
+            justifyContent: "center",
+            background: "linear-gradient(to top, rgba(100,0,0,.9), rgba(100,0,0,0.3))",
+            borderRadius: "12px",
+          }}
+        >
+          <div
+            style={{
+              width: "160vh",
+              border: "2px solid black",
+              borderRadius: "12px",
+              alignItems: "center",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              backgroundColor: "#a02727",
+            }}
+          >
+            <h2 style={{ color: "white" }}>All Sanctions</h2>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "32px",
+              padding: "32px",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            {sanctions.map((sanction) => (
+              <div
+                style={{
+                  border: "1px black solid",
+                  width: "80vh",
+                  borderRadius: "15px",
+                  background: "linear-gradient(to right, #a43737 40%, #e8bd26 95%)",
+                }}
+              >
+                <li key={sanction.sanction_id} style={{ listStyle: "none" }}>
+                  {/* Display sanction details */}
+                  <div
+                    style={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      color: "white",
+                      padding: "10px",
+                    }}
+                  >
+                    <div
+                      style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "flex-start",
+                        padding: "8px",
+                        gap: "8px",
+                      }}
+                    >
+                      <div>Sanction ID: {sanction.sanction_id}</div>
+                      <div>
+                        Student Name: {sanction.student.firstname} {sanction.student.lastname}
+                      </div>
+                      <div>Behavior Details: {sanction.behaviorDetails}</div>
+                      <div>Sanction Recommendation: {sanction.sanctionRecommendation}</div>
+                    </div>
+                    {/* Approve and decline buttons */}
+                    <div style={{ display: "flex", flexDirection: "column" }}>
+                      {sanction.isApproved !== 1 && (
+                        <div style={{ display: "flex", gap: "8px" }}>
+                          <button onClick={() => handleApprove(sanction.sanction_id)}>
+                            Approve
+                          </button>
+                          <button onClick={() => handleDecline(sanction.sanction_id)}>
+                            Decline
+                          </button>
+                        </div>
+                      )}
+                      {/* View associated cases, sanctions, and records */}
+                      <div>
+                        <Link
+                          onMouseEnter={(e) => (e.target.style.color = "black")}
+                          onMouseLeave={(e) => (e.target.style.color = "white")}
+                          style={{ color: "white", fontWeight: "bold" }}
+                          to={`/view-student-cases/${sanction.sid}`}
+                        >
+                          View Cases
+                        </Link>
+                        <br></br>
+                        <Link
+                          onMouseEnter={(e) => (e.target.style.color = "black")}
+                          onMouseLeave={(e) => (e.target.style.color = "white")}
+                          style={{ color: "white", fontWeight: "bold" }}
+                          to={`/view-student-sanctions/${sanction.sid}`}
+                        >
+                          View Sanctions
+                        </Link>
+                        <br></br>
+                        <Link
+                          onMouseEnter={(e) => (e.target.style.color = "black")}
+                          onMouseLeave={(e) => (e.target.style.color = "white")}
+                          style={{ color: "white", fontWeight: "bold" }}
+                          to={`/view-student-report/${sanction.sid}`}
+                        >
+                          View Records
+                        </Link>
+                        <br></br>
+                      </div>
+                    </div>
                   </div>
-                )}
-                {/* View associated cases, sanctions, and records */}
-                <div>
-                  <Link to={`/view-student-cases/${sanction.sid}`}>View Cases</Link><br></br>
-                  <Link to={`/view-student-sanctions/${sanction.sid}`}>View Sanctions</Link><br></br>
-                  <Link to={`/view-student-report/${sanction.sid}`}>View Records</Link><br></br>
-                </div>
-              </li>
+                </li>
+              </div>
             ))}
-          </ul>
+          </div>
         </div>
       </div>
     </div>
   );
-  
-}
+};
 
 export default ViewSanctions;
