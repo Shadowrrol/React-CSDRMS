ddfb7c425f76ee29d756b893a1eb085e886ff874
Andreiyaa1 <143610003+Andreiyaa1@users.noreply.github.com>
2024-05-21 00:22:38 +0800

    view sanction update

--- src/Principal/ViewSanctions.js
+++ src/Principal/ViewSanctions.js
@@ -4,8 +4,76 @@ import { Link, useNavigate } from "react-router-dom";
 import styles from "../Navigation.module.css"; // Import CSS module
 
 import AssessmentIcon from "@mui/icons-material/Assessment";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import LocalPoliceIcon from "@mui/icons-material/LocalPolice";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 
+function ButtonMenu({ sanctionId, index }) {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  return (
+    <div>
+      <Button
+        id="basic-button"
+        aria-controls={open ? "basic-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
+        style={{
+          backgroundColor: "#861b1c",
+          color: "white",
+          fontWeight: "bold",
+          border: "none",
+        }}
+        endIcon={<KeyboardArrowDownIcon />}
+      >
+        Supporting Documents
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+      >
+        <MenuItem onClick={handleClose}>
+          <Link
+            style={{ color: "inherit", textDecoration: "none" }}
+            to={`/view-student-cases/${sanctionId}`}
+          >
+            View Cases
+          </Link>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Link
+            style={{ color: "inherit", textDecoration: "none" }}
+            to={`/view-student-report/${sanctionId}`}
+          >
+            View Records
+          </Link>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Link
+            style={{ color: "inherit", textDecoration: "none" }}
+            to={`/view-student-sanctions/${sanctionId}`}
+          >
+            View Sanctions
+          </Link>
+        </MenuItem>
+      </Menu>
+    </div>
+  );
+}
 const ViewSanctions = () => {
   const navigate = useNavigate();
   const [sanctions, setSanctions] = useState([]);
@@ -125,132 +193,75 @@ const ViewSanctions = () => {
             }}
           >
             {sanctions.map((sanction) => (
-              <div style={{ width: "90hv", display: "flex" }}>
-                <div
-                  style={{
-                    border: "1px black solid",
-                    width: "80vh",
-                    borderRadius: "15px",
-                    background: "linear-gradient(to right, #a43737 40%, #e8bd26 95%)",
-                  }}
-                >
-                  <li key={sanction.sanction_id} style={{ listStyle: "none" }}>
-                    {/* Display sanction details */}
+              <div
+                style={{
+                  border: "1px black solid",
+                  width: "80vh",
+                  borderRadius: "15px",
+                  background: "linear-gradient(to right, #a43737 40%, #e8bd26 95%)",
+                }}
+              >
+                <li key={sanction.sanction_id} style={{ listStyle: "none" }}>
+                  {/* Display sanction details */}
+                  <div
+                    style={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      color: "white",
+                      padding: "10px",
+                    }}
+                  >
                     <div
                       style={{
                         display: "flex",
-                        justifyContent: "space-between",
-                        color: "white",
-                        padding: "10px",
+                        flexDirection: "column",
+                        alignItems: "flex-start",
+                        padding: "8px",
+                        gap: "8px",
                       }}
                     >
-                      <div
-                        style={{
-                          display: "flex",
-                          flexDirection: "column",
-                          alignItems: "flex-start",
-                          padding: "8px",
-                          gap: "8px",
-                        }}
-                      >
-                        <div>Sanction ID: {sanction.sanction_id}</div>
-                        <div>
-                          Student Name: {sanction.student.firstname} {sanction.student.lastname}
-                        </div>
-                        <div>Behavior Details: {sanction.behaviorDetails}</div>
-                        <div>Sanction Recommendation: {sanction.sanctionRecommendation}</div>
+                      <div>Sanction ID: {sanction.sanction_id}</div>
+                      <div>
+                        Student Name: {sanction.student.firstname} {sanction.student.lastname}
                       </div>
-                      {/* Approve and decline buttons */}
+                      <div>Behavior Details: {sanction.behaviorDetails}</div>
+                      <div>Sanction Recommendation: {sanction.sanctionRecommendation}</div>
+                    </div>
+                    {/* Approve and decline buttons */}
+                    <div
+                      style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    >
+                      {sanction.isApproved !== 1 && (
+                        <div style={{ display: "flex", gap: "8px" }}>
+                          <button onClick={() => handleApprove(sanction.sanction_id)}>
+                            Approve
+                          </button>
+                          <button onClick={() => handleDecline(sanction.sanction_id)}>
+                            Decline
+                          </button>
+                        </div>
+                      )}
+                      {/* View associated cases, sanctions, and records */}
+
                       <div
                         style={{
                           display: "flex",
                           flexDirection: "column",
+                          alignItems: "start",
                           justifyContent: "center",
-                          alignItems: "center",
-                          marginTop: "15px",
+                          marginLeft: "4px",
                         }}
                       >
-                        {sanction.isApproved !== 1 && (
-                          <div style={{ display: "flex", gap: "8px" }}>
-                            <button onClick={() => handleApprove(sanction.sanction_id)}>
-                              Approve
-                            </button>
-                            <button onClick={() => handleDecline(sanction.sanction_id)}>
-                              Decline
-                            </button>
-                          </div>
-                        )}
-                        {/* View associated cases, sanctions, and records */}
+                        <ButtonMenu key={sanction.id} sanctionId={sanction.sid} />
                       </div>
                     </div>
-                  </li>
-                </div>
-
-                <div
-                  style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "start",
-                    justifyContent: "center",
-                    marginLeft: "4px",
-                  }}
-                >
-                  <button
-                    style={{
-                      color: "white",
-                      fontWeight: "bold",
-                      backgroundColor: "#861b1c;",
-                      border: "none",
-                      cursor: "pointer",
-                      padding: "8px",
-                      fontSize: "16px",
-                    }}
-                  >
-                    <Link
-                      style={{ color: "inherit", textDecoration: "none" }}
-                      to={`/view-student-cases/${sanction.sid}`}
-                    >
-                      View Cases
-                    </Link>
-                  </button>
-
-                  <button
-                    style={{
-                      color: "white",
-                      fontWeight: "bold",
-                      backgroundColor: "#861b1c;",
-                      border: "none",
-                      cursor: "pointer",
-                      padding: "8px",
-                      fontSize: "16px",
-                    }}
-                  >
-                    <Link
-                      style={{ color: "inherit", textDecoration: "none" }}
-                      to={`/view-student-report/${sanction.sid}`}
-                    >
-                      View Records
-                    </Link>
-                  </button>
-                  <button
-                    style={{
-                      color: "white",
-                      fontWeight: "bold",
-                      backgroundColor: "#861b1c;",
-                      border: "none",
-                      cursor: "pointer",
-                      padding: "8px",
-                      fontSize: "16px",
-                    }}
-                  >
-                    <Link
-                      style={{ color: "inherit", textDecoration: "none" }}
-                      to={`/view-student-sanctions/${sanction.sid}`}
-                    >
-                      View Sanctions
-                    </Link>
-                  </button>
-                </div>
+                  </div>
+                </li>
               </div>
             ))}
           </div>
