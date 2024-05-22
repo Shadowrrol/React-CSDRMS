c921dfcbb33de8b8143d23f5a0200a1629887138
Katarina <138646000+BrittMichaels@users.noreply.github.com>
2024-05-22 23:43:12 +0800

    dashboard and smol change

--- src/Admin/AdminDashboard.js
+++ src/Admin/AdminDashboard.js
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom'; // Import useNavigate hook
+import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import styles from '../Navigation.module.css';
-
+import navStyles from '../Navigation.module.css';
+import styles from './admindashboard.module.css';
 import MenuPopupState from '../components/MenuPopupState';
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
 import MeetingRoomIcon from '@mui/icons-material/MeetingRoom';
@@ -11,7 +10,8 @@ import AssessmentIcon from '@mui/icons-material/Assessment';
 
 const AdminDashboard = () => {
   const [users, setUsers] = useState([]);
-  const navigate = useNavigate(); // Initialize useNavigate hook
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
 
   const fetchUsers = async () => {
     try {
@@ -19,6 +19,8 @@ const AdminDashboard = () => {
       setUsers(response.data);
     } catch (error) {
       console.error('Error fetching users:', error);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -28,60 +30,65 @@ const AdminDashboard = () => {
   }, []);
 
   const handleUpdateUser = (user) => {
-    navigate(`/UpdateAccount`, { state: { user } }); // Navigate to UpdateAccount with user state
+    navigate(`/UpdateAccount`, { state: { user } });
   };
 
   const deleteUser = async (username) => {
     try {
       await axios.delete(`http://localhost:8080/user/deleteUser/${username}`);
-      // Refresh the user list after deleting a user
       fetchUsers();
     } catch (error) {
       console.error('Error deleting user:', error);
     }
   };
 
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div className={styles.wrapper} style={{ backgroundImage: 'url(/public/image-2-3@2x.png)' }}>
-      <div className={styles.sidenav}>
-        <img src="/image-removebg-preview (1).png" alt="" className={styles['sidebar-logo']} />
-        <Link to="/AdminDashboard" className={styles['styled-link']}>
-          <AssessmentIcon className={styles.icon} />
-          <span className={styles['link-text']}>Dashboard</span>
+    <div className={navStyles.wrapper} style={{ backgroundImage: 'url(/public/image-2-3@2x.png)' }}>
+      <div className={navStyles.sidenav}>
+        <img src="/image-removebg-preview (1).png" alt="" className={navStyles['sidebar-logo']} />
+        <Link to="/AdminDashboard" className={navStyles['styled-link']}>
+          <AssessmentIcon className={navStyles.icon} />
+          <span className={navStyles['link-text']}>Dashboard</span>
         </Link>
-        <Link to="/account" className={styles['styled-link']}>
-          <AccountBoxIcon className={styles.icon} />
-          <span className={styles['link-text']}>Account</span>
+        <Link to="/account" className={navStyles['styled-link']}>
+          <AccountBoxIcon className={navStyles.icon} />
+          <span className={navStyles['link-text']}>Account</span>
         </Link>
-        <Link to="/class" className={styles['styled-link']}>
-          <MeetingRoomIcon className={styles.icon} />
-          <span className={styles['link-text']}>Class</span>
+        <Link to="/class" className={navStyles['styled-link']}>
+          <MeetingRoomIcon className={navStyles.icon} />
+          <span className={navStyles['link-text']}>Class</span>
         </Link>
         <MenuPopupState />
       </div>
-      <div className='content'>
-        <h1>All Users</h1>
-        <table>
-          <thead>
-            <tr>
-              <th>Username</th>
-              <th>User Type</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map(user => (
-              <tr key={user.username}>
-                <td>{user.username}</td>
-                <td>{user.userType}</td>
-                <td>
-                  <button onClick={() => handleUpdateUser(user)}>Update</button> {/* Use onClick event to trigger navigation */}
-                  <button onClick={() => deleteUser(user.username)}>Delete</button>
-                </td>
+      <div className={styles.contentContainer}>
+        <div className={styles.content}>
+          <h1 className={styles.h2}>All Users</h1>
+          <table>
+            <thead>
+              <tr>
+                <th>Username</th>
+                <th>User Type</th>
+                <th>Action</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {users.map(user => (
+                <tr key={user.username}>
+                  <td>{user.username}</td>
+                  <td>{user.userType}</td>
+                  <td className={styles['action-buttons']}>
+                    <button className={styles.button} onClick={() => handleUpdateUser(user)}>Update</button>
+                    <button className={styles.button} onClick={() => deleteUser(user.username)}>Delete</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
